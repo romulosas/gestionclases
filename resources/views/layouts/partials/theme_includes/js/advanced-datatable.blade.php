637b1597236b156defd5622147bbc82c4ad2d2bd
Romulo <romulosas@gmail.com>
2019-10-06 21:53:51 -0300

    Change app layout name to dashboard, create a new default layout and add course, state of course controllers

---
+++ resources/views/layouts/partials/theme_includes/js/advanced-datatable.blade.php
@@ -0,0 +1,11 @@
+/**
+ * Created by Romulo Sant´Ana.
+ * User: c#
+ * Date: 06-10-2019
+ * Time: 18:39
+ */
+
+<!-- dynamic table -->
+<link href="{{ asset('theme/assets/advanced-datatable/media/css/demo_page.css') }}" rel="stylesheet">
+<link href="{{ asset('theme/assets/advanced-datatable/media/css/demo_table.css') }}" rel="stylesheet">
+<link href="{{ asset('theme/assets/data-tables/DT_bootstrap.css') }}" rel="stylesheet">
