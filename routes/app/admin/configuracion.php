66eed2d786ad4fa263a76e4fbe66ccc06f1a632f
Romulo <romulosas@gmail.com>
2019-10-06 21:57:00 -0300

    Add course and state course routes

--- routes/app/admin/configuracion.php
+++ routes/app/admin/configuracion.php
@@ -1,13 +1,17 @@
 <?php
 
 
-Route::group([ 'middleware' => 'auth'], function () {
+Route::group([ 'middleware' => 'auth','verified'], function () {
 
     Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
 
         Route::group(['namespace' => 'Configuracion', 'prefix' => 'configuracion'], function () {
 
+            Route::group(['namespace' => 'Curso'], function () {
 
+                Route::resource('curso', 'CursoController');
+                Route::resource('estadocurso', 'EstadoCursoController');
+            });
 
         });
 
