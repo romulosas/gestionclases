fb12acb77e2abea66a4f57bee94002ac166f1683
Romulo <romulosas@gmail.com>
2019-10-06 21:57:28 -0300

    Add Course state controller

---
+++ app/Http/Controllers/Admin/Configuracion/Curso/EstadoCursoController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Configuracion\Curso;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\AppController;
+
+class EstadoCursoController extends AppController
+{
+    public function __construct()
+    {
+        $this->setTitle("Estado de Curso");
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return View('admin.configuracion.curso.estadocurso.index', ['viewTitle' => $this->viewTitle]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
